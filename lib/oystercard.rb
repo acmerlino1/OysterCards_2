cbeb5e86899a37b5ac6102cbb77240f8650a7e14
AliciaMerlino <acmerlino1@gmail.com>
2019-12-17 12:31:43 +0000

    copy of repository. Oystercard can be topped up to 90 pounds, tapped on and off, and money deducted after each journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,28 +1,29 @@
 class Oystercard
-  MAXIMUN = 90
+  MAXIMUM = 90
   MINIMUM = 1
   MIN_CHARGE = 1
+  
   ERROR = {
-    max: 'Maximun allowance reached'.freeze,
+    max: 'Maximum allowance reached'.freeze,
     min: 'raise_error if below min amount'
    }
-  attr_reader :balance
-  attr_reader :entry_station
+  attr_reader :balance, :entry_station, :in_journey
 
     def initialize
       @balance = 0
     end
 
     def top_up(amount)
-      fail ERROR[:max] if @balance >= MAXIMUN
+      fail ERROR[:max] if @balance == MAXIMUM
 
       @balance += amount
     end
 
-    def touch_in(station)
+    def touch_in
       fail ERROR[:min] if @balance < MINIMUM
+      @in_journey = true
 
-      @entry_station = station
+      # @entry_station = station
 #this was failing due to not having a amount
 #to succcssfully test top_up(1)
 
@@ -34,9 +35,11 @@ class Oystercard
     end
 
     def in_journey?
-      !!entry_station
+      false # !!entry_station
     end
-private
+
+    # private
+    
     def reduce(amount)
       @balance -= amount
     end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,18 @@
 require 'oystercard'
 
 describe Oystercard do
+  
   let(:station){ double :station }
-  it 'stores the entry station' do
-    subject.top_up(1)
-    subject.touch_in(station)
-    expect(subject.entry_station).to eq station
-  end
-  it 'has a balance of zero' do
-    expect(subject.balance).to eq(0)
-  end
+    
+    # it 'stores the entry station' do
+    #   subject.top_up(1)
+    #   subject.touch_in(station)
+    #   expect(subject.entry_station).to eq station
+    # end
+
+    it 'has a balance of zero' do
+      expect(subject.balance).to eq(0)
+    end
 
   describe '#top_up' do
       it { is_expected.to respond_to(:top_up).with(1).argument }
@@ -18,10 +21,11 @@ describe Oystercard do
         expect{ subject.top_up 1}.to change{ subject.balance }.by 1
       end
 
-      it 'Maximun allowance reached'do
-      Oystercard::MAXIMUN.times {subject.top_up(1)}
-      expect {subject.top_up(@balance)}.to raise_error(Oystercard::ERROR[:max])
+      it 'Maximum allowance reached'do
+      Oystercard::MAXIMUM.times {subject.top_up(1)}
+      expect {subject.top_up(1)}.to raise_error(Oystercard::ERROR[:max])
     end
+  end
 
 
   # describe '#reduce' do
@@ -33,33 +37,43 @@ describe Oystercard do
 # end
 
   describe '#touch_in' do
+
+    it 'is initially not in a journey' do
+      expect(subject).not_to be_in_journey
+    end
+
+    it 'can touch in' do
+      subject.top_up(Oystercard::MIN_CHARGE)
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
     it 'raise_error if below min amount' do
-      expect {subject.touch_in(station)}.to raise_error(Oystercard::ERROR[:min])
+      expect {subject.touch_in}.to raise_error(Oystercard::ERROR[:min])
     end
 
     it 'reduce card amount by min_charge' do
       subject.top_up(5)
-      subject.touch_in(station)
+      subject.touch_in
       expect{ subject.touch_out }.to change{ subject.balance }.by(-Oystercard::MIN_CHARGE)
     end
-
-  describe '#in_journey' do
-  it 'returns true if in journey' do
-    allow(subject).to receive(:entry_station).and_return(true)
-    expect(subject.in_journey?).to eq(true)
   end
 
-  it 'returns false if not in journey' do
-    allow(subject).to receive(:entry_station).and_return(false)
-    expect(subject.in_journey?).to eq(false)
-  end
+  # describe '#in_journey' do
+  #   it 'returns true if in journey' do
+  #     allow(subject).to receive(:entry_station).and_return(true)
+  #     expect(subject.in_journey?).to eq(true)
+  #   end
+
+  #   it 'returns false if not in journey' do
+  #     allow(subject).to receive(:entry_station).and_return(false)
+  #     expect(subject.in_journey?).to eq(false)
+  #   end
+  # end
 
   describe '#touch_out' do
-  it 'reduces by minimum amount' do
-    expect {subject.touch_out}.to change { subject.balance }.by -1
-end
-end
-end
-end
-end
+    it 'reduces by minimum amount' do
+      expect {subject.touch_out}.to change { subject.balance }.by -1
+    end
+  end
 end
